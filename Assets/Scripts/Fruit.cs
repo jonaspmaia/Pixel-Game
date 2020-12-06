c94c817353da819579a260ae3a01c163d92ae44b
Jonas Pires Maia <jonasmaia7@hotmail.com>
2020-12-05 22:26:34 -0300

    First Commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
+    public int score;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            GameController.instance.totalScore += score;
+
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int totalScore;
+
+    public static GameController instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+}
